58625d3755d54e062bf827b44bb9cedae707949c
santiaago <santiago.arias@red-gate.com>
2017-07-21 16:40:03 +0100

    call event source

--- app/src/App.js
+++ app/src/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      dots: {}
+    }
+    var es = new EventSource('http://localhost:8081/api/sse')
+    es.onmessage = function(event){
+      console.log(event.data)
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className='App'>
+        <div className='App-header'>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <canvas width='500' height='500' />
       </div>
     );
   }
