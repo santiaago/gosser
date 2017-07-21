e8756a3e37df4537c7bd9eaf3ca6bc0a8edaf1e9
santiaago <santiago.arias@red-gate.com>
2017-07-21 15:30:33 +0100

    sse broker

--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,160 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+// the amount of time to wait when pushing a message to
+// a slow client or a client that closed after `range clients` started.
+const patience time.Duration = time.Second * 1
+
+// Example SSE server in Golang.
+//     $ go run sse.go
+
+type Broker struct {
+
+	// Events are pushed to this channel by the main events-gathering routine
+	Notifier chan []byte
+
+	// New client connections
+	newClients chan chan []byte
+
+	// Closed client connections
+	closingClients chan chan []byte
+
+	// Client connections registry
+	clients map[chan []byte]bool
+}
+
+func NewServer() (broker *Broker) {
+	// Instantiate a broker
+	broker = &Broker{
+		Notifier:       make(chan []byte, 1),
+		newClients:     make(chan chan []byte),
+		closingClients: make(chan chan []byte),
+		clients:        make(map[chan []byte]bool),
+	}
+
+	// Set it running - listening and broadcasting events
+	go broker.listen()
+
+	return
+}
+
+func (broker *Broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// Make sure that the writer supports flushing.
+	//
+	flusher, ok := w.(http.Flusher)
+
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Each connection registers its own message channel with the Broker's connections registry
+	messageChan := make(chan []byte)
+
+	// Signal the broker that we have a new connection
+	broker.newClients <- messageChan
+
+	// Remove this client from the map of connected clients
+	// when this handler exits.
+	defer func() {
+		broker.closingClients <- messageChan
+	}()
+
+	// Listen to connection close and un-register messageChan
+	notify := w.(http.CloseNotifier).CloseNotify()
+
+	for {
+		select {
+		case <-notify:
+			return
+		default:
+
+			// Write to the ResponseWriter
+			// Server Sent Events compatible
+			data := struct {
+				Type string  `json:"type"`
+				Time string  `json:"time"`
+				X    float64 `json:"x"`
+				Y    float64 `json:"y"`
+			}{
+				"position",
+				fmt.Sprintf("%s", <-messageChan),
+				rand.Float64(),
+				rand.Float64(),
+			}
+
+			if err := json.NewEncoder(w).Encode(data); err != nil {
+				log.Println(err)
+				http.Error(w, "unable to encode data", http.StatusInternalServerError)
+			}
+
+			// Flush the data immediatly instead of buffering it for later.
+			flusher.Flush()
+		}
+	}
+
+}
+
+func (broker *Broker) listen() {
+	for {
+		select {
+		case s := <-broker.newClients:
+
+			// A new client has connected.
+			// Register their message channel
+			broker.clients[s] = true
+			log.Printf("Client added. %d registered clients", len(broker.clients))
+		case s := <-broker.closingClients:
+
+			// A client has dettached and we want to
+			// stop sending them messages.
+			delete(broker.clients, s)
+			log.Printf("Removed client. %d registered clients", len(broker.clients))
+		case event := <-broker.Notifier:
+
+			// We got a new event from the outside!
+			// Send event to all connected clients
+			for clientMessageChan := range broker.clients {
+				select {
+				case clientMessageChan <- event:
+				case <-time.After(patience):
+					log.Print("Skipping client.")
+				}
+			}
+		}
+	}
+
+}
 
 func main() {
 	log.Println("staring server at 8081..")
 
 	http.HandleFunc("/api/hello", hello)
+	broker := NewServer()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			eventString := fmt.Sprintf("%v", time.Now())
+			log.Println("Receiving event")
+			broker.Notifier <- []byte(eventString)
+		}
+	}()
+
+	http.HandleFunc("/api/sse", broker.ServeHTTP)
 	http.ListenAndServe(":8081", nil)
 }
 
