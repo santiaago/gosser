b36c76d8ba5bbf525f463f46b76786b6cb7aa130
santiaago <santiago.ariassar@gmail.com>
2017-07-22 15:42:16 +0100

    draw dot on new message

--- app/src/App.js
+++ app/src/App.js
@@ -9,8 +9,21 @@ class App extends Component {
     }
     var es = new EventSource('http://localhost:8081/api/sse')
     es.onmessage = function(event){
-      console.log(event.data)
-    }
+      const msg = JSON.parse(event.data)
+
+      const dots = Object.assign({}, this.state.dots)
+      dots[msg.id] = msg
+      this.setState({dots: dots})
+    }.bind(this)
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const ctx = this.refs.canvas.getContext('2d')
+    ctx.clearRect(0, 0, 500, 500)
+    Object.keys(this.state.dots).forEach(k => {
+      const d = this.state.dots[k]
+      ctx.fillRect(d.x * 100, d.y * 100, 10, 10)
+    })
   }
 
   render() {
@@ -22,7 +35,7 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <canvas width='500' height='500' />
+        <canvas ref='canvas' width='500' height='500' />
       </div>
     );
   }
